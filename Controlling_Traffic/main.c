f1879edcd01e61086fb16e410fd92786296094f5
Lakshay Negi <78898582+LakshayNegi108@users.noreply.github.com>
2021-08-03 21:03:27 +0530

    main.c

--- Controlling_Traffic/main.c
+++ Controlling_Traffic/main.c
@@ -1,98 +1,76 @@
-#include <msp430.h> 
+//Single Lane Traffic light control
+
+#include <msp430.h>
+
+#define SW_east  BIT3
+#define SW_west  BIT4
+
+#define red_east BIT0
+#define yellow_east BIT1
+#define green_east BIT2
+
+#define red_west BIT3
+#define yellow_west BIT4
+#define green_west BIT5
 
 
-#define East_Button BIT0
-#define West_Button BIT2
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD;
 
-#define eastRed BIT0
-#define eastYellow BIT1
-#define eastGreen BIT2
-#define westRed BIT3
-#define westYellow BIT4
-#define westGreen BIT5
+    P1DIR |= red_east|yellow_east|green_east|red_west|yellow_west|green_west;
+    P2DIR &= ~SW_east|SW_west;
+    volatile int traffic_west = 1;
+   // const unsigned int flowtime =10000000;
+   // const unsigned int changedelay = 1100000;
+    unsigned int i;
 
+    P1OUT |= red_east|green_west;
+    P1OUT &= ~(red_west|yellow_west|yellow_east|green_east);
 
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	P1DIR &= ~East_Button;      //Set East Button -> Input
-	P1DIR &= ~West_Button;      //Set West Button -> Input
+    while(1)
+    {
 
-	P2DIR |= eastRed;           //Set red LED on east -> Output
-	P2DIR |= eastYellow;        //Set Yellow LED on east -> Output
-	P2DIR |= eastGreen;         //Set Green LED on east -> Output
-	P2DIR |= westRed;           //Set red LED on west -> Output
-	P2DIR |= westYellow;        //Set Yellow LED on west -> Output
-	P2DIR |= westGreen;         //Set Green LED on west -> Output
+        if(P2IN & SW_east){
 
-	P2OUT ^= westGreen;
-	P2OUT ^= eastRed;
+                    if(traffic_west == 0){
+                        traffic_west = 1;
+                        __delay_cycles(10000000);
+                        P1OUT &= ~green_east;
+                        P1OUT |= yellow_east;
+                        __delay_cycles(4100000);
+                        P1OUT &= ~yellow_east;
+                        P1OUT |= red_east;
+                        __delay_cycles(4100000);
+                        for(i=5;i>0;i--){
+                            P1OUT |= yellow_west;
+                            __delay_cycles(500000);
+                            P1OUT &= ~yellow_west;
+                            __delay_cycles(500000);
+                        }
+                        P1OUT &= ~(red_west|yellow_west);
+                        P1OUT |= green_west;
+                    }
+                }
+        if(P2IN & SW_west){
 
-	int trafficWest = 1;
-	const unsigned int flowtime = 10000000;
-	const unsigned int changedelay = 500000;
-	int a,b,i;
-
-	while(1){
-         if(!(P1IN & West_Button)){
-             __delay_cycles(20000);  // Wait 20ms to debounce
-              while(!(P1IN & West_Button));    // Wait till SW Released
-             __delay_cycles(20000);
-             if(trafficWest != 1){
-                 trafficWest = 1;
-              //   __delay_cycles(flowtime);
-                 for(i=0;i<flowtime;i++);
-                 for(i=0;i<flowtime;i++);
-                 P2OUT &= ~eastGreen;
-
-                 P2OUT |= eastYellow;
-                 __delay_cycles(changedelay);
-                 P2OUT &= ~eastYellow;
-
-                 P2OUT |= eastRed;
-                 __delay_cycles(changedelay);
-                 for(a = 0;a<5;a++){
-                     P2OUT &= ~westYellow;
-                     __delay_cycles(changedelay);
-                     P2OUT |= westYellow;
-                     __delay_cycles(changedelay);
-                 }
-                 P2OUT &= ~westYellow;
-                 P2OUT &= ~westRed;
-                 P2OUT |= westGreen;
-             }
-         }
-         if(!(P1IN & East_Button)){
-             __delay_cycles(20000);  // Wait 20ms to debounce
-              while(!(P1IN & East_Button));    // Wait till SW Released
-              __delay_cycles(20000);
-             if(trafficWest == 1){
-                 trafficWest = 0;
-             //    __delay_cycles(flowtime);
-                 for(i=0;i<flowtime;i++);
-                 for(i=0;i<flowtime;i++);
-
-                 P2OUT &= ~westGreen;
-                 P2OUT |= westYellow;
-                 __delay_cycles(changedelay);
-
-                 P2OUT &= ~westYellow;
-                 P2OUT |= westRed;
-                 __delay_cycles(changedelay);
-
-                 for(b=0;b<5;b++){
-                     P2OUT &= ~eastYellow;
-                     __delay_cycles(changedelay);
-                     P2OUT |= eastYellow;
-                     __delay_cycles(changedelay);
-                 }
-                 P2OUT &= ~eastYellow;
-                 P2OUT &= ~eastRed;
-                 P2OUT |= eastGreen;
-             }
-         }
-	}
-
-
-	return 0;
+                  if(traffic_west == 1){
+                      traffic_west = 0;
+                      __delay_cycles(10000000);
+                      P1OUT &= ~green_west;
+                      P1OUT |= yellow_west;
+                      __delay_cycles(4100000);
+                      P1OUT &= ~yellow_west;
+                      P1OUT |= red_west;
+                      __delay_cycles(4100000);
+                      for(i=5;i>0;i--){
+                          P1OUT |= yellow_east;
+                          __delay_cycles(500000);
+                          P1OUT &= ~yellow_east;
+                          __delay_cycles(500000);
+                      }
+                      P1OUT &= ~(red_east|yellow_east);
+                      P1OUT |= green_east;
+                  }
+              }
+    }
 }
