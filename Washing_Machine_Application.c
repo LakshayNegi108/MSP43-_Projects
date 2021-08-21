01404e39f6208e7e0594022e59fe1509a8e36747
Lakshay Negi <78898582+LakshayNegi108@users.noreply.github.com>
2021-08-21 22:46:29 +0530

    Update Washing_Machine_Application.c

--- Washing_Machine_Application.c
+++ Washing_Machine_Application.c
@@ -11,114 +11,134 @@
 #define PreWash 2
 #define FinalSpin 3
 
- int Program=0;
- int RotationSpeed = 0;
- int open = 0;
+int Program=0;
+int RotationSpeed = 0;
+int open = 0;
 
 void delay_ms(int);
 void Wash();
 
 void main(void)
 {
- WDTCTL = WDTPW|WDTHOLD;
+    WDTCTL = WDTPW|WDTHOLD;
 
- P2SEL = 0x00;
- P1DIR = 0xFF;
- P2DIR = 0x83;
- P2REN = 0x7C;
- P1OUT = 0x00;
- P2OUT = 0x7C;
- P2IE = 0x7C;
- P2IES = 0x7C;
- P2IFG = 0x00;
+    P2SEL = 0x00;
+    P1DIR = 0xFF;
+    P2DIR = 0x83;
+    P2REN = 0x7C;
+    P1OUT = 0x00;
+    P2OUT = 0x7C;
+    P2IE = 0x7C;
+    P2IES = 0x7C;
+    P2IFG = 0x00;
 
- __bis_SR_register(GIE);
+    __bis_SR_register(GIE);
 
- while(1){
- if(Program!=0) Wash();
- }
+    while(1)
+    {
+        if(Program!=0) Wash();
+    }
 }
 
-void delay_ms(int a){
- while(a != 0)
- {
- _delay_cycles(1000);
- a--;
- }
+void delay_ms(int a)
+{
+    while(a != 0)
+    {
+        _delay_cycles(1000);
+        a--;
+    }
 }
 
-void Wash(){
- int speed, turn, fast, slow, Rturn, Lturn, pos;
- volatile unsigned int seqr[8] = {0x08,0x0C,0x04,0x06,\
- 0x02,0x03,0x01,0x09};
- volatile unsigned int seql[8] = {0x09,0x01,0x03,0x02,\
- 0x06,0x04,0x0C,0x08};
+void Wash()
+{
+    int speed, turn, fast, slow, Rturn, Lturn, pos;
+    volatile unsigned int seqr[8] = {0x08,0x0C,0x04,0x06,\
+                                     0x02,0x03,0x01,0x09
+                                    };
+    volatile unsigned int seql[8] = {0x09,0x01,0x03,0x02,\
+                                     0x06,0x04,0x0C,0x08
+                                    };
 
- if (Program == 1){
- slow = 20, fast = 10, Rturn = 100, Lturn = 100;
- }
- if (Program == 2){
- slow = 20, fast = 10, Rturn = 30, Lturn = 30;
- }
- if (Program == 3){
- slow = 10, fast = 5, Rturn = 50, Lturn = 0;
- }
+    if (Program == 1)
+    {
+        slow = 20, fast = 10, Rturn = 100, Lturn = 100;
+    }
+    if (Program == 2)
+    {
+        slow = 20, fast = 10, Rturn = 30, Lturn = 30;
+    }
+    if (Program == 3)
+    {
+        slow = 10, fast = 5, Rturn = 50, Lturn = 0;
+    }
 
- if(RotationSpeed == 0)
- speed = fast;
- else
- speed = slow;
+    if(RotationSpeed == 0)
+        speed = fast;
+    else
+        speed = slow;
 
- for (turn=0; turn<Rturn; turn++){
- if(open == 1){
- pos = 0;
- while(pos<8){
- P1OUT = seqr[pos];
- pos++;
- delay_ms(speed);
- }
- P1OUT = 0x00;
- }
- else break;
- }
- for (turn=0; turn<Lturn; turn++){
- if(open == 1){
- pos = 0;
- while(pos<8){
- P1OUT = seql[pos];
- pos++;
- delay_ms(speed);
- }
- P1OUT = 0x00;
- }
- else break;
- }
- Program = 0;
+    for (turn=0; turn<Rturn; turn++)
+    {
+        if(open == 1)
+        {
+            pos = 0;
+            while(pos<8)
+            {
+                P1OUT = seqr[pos];
+                pos++;
+                delay_ms(speed);
+            }
+            P1OUT = 0x00;
+        }
+        else break;
+    }
+    for (turn=0; turn<Lturn; turn++)
+    {
+        if(open == 1)
+        {
+            pos = 0;
+            while(pos<8)
+            {
+                P1OUT = seql[pos];
+                pos++;
+                delay_ms(speed);
+            }
+            P1OUT = 0x00;
+        }
+        else break;
+    }
+    Program = 0;
 }
 
 #pragma vector =PORT2_VECTOR
-__interrupt void PORT2_ISR(void){
- if(ONOFF){
- open ^= 1;
- RedLedToggle;
- P2IFG &= ~0x40;
- }
- if ((RSPEED) && (open == 1)){
- RotationSpeed ^= 1;
- YellowLedToggle;
- P2IFG &= ~0x04;
- }
- if ((NWASH) && (open == 1)){
- Program = NormalWash;
- P2IFG &= ~0x08;
- }
- if ((PWASH) && (open == 1)){
- Program = PreWash;
- P2IFG &= ~0x10;
- }
- if ((FSPIN) && (open == 1)){
- Program = FinalSpin;
- P2IFG &= ~0x20;
- }
- P2IFG=0x00;
+__interrupt void PORT2_ISR(void)
+{
+    if(ONOFF)
+    {
+        open ^= 1;
+        RedLedToggle;
+        P2IFG &= ~0x40;
+    }
+    if ((RSPEED) && (open == 1))
+    {
+        RotationSpeed ^= 1;
+        YellowLedToggle;
+        P2IFG &= ~0x04;
+    }
+    if ((NWASH) && (open == 1))
+    {
+        Program = NormalWash;
+        P2IFG &= ~0x08;
+    }
+    if ((PWASH) && (open == 1))
+    {
+        Program = PreWash;
+        P2IFG &= ~0x10;
+    }
+    if ((FSPIN) && (open == 1))
+    {
+        Program = FinalSpin;
+        P2IFG &= ~0x20;
+    }
+    P2IFG=0x00;
 }
